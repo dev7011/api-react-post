84f50224d3c2c73ac8414e2dbeae6887ba2c801b
ntle <tnle2017@gmail.com>
2026-08-02 18:26:13 +0200

    chg: Add PostsList component with initial state and API fetch setup

--- client/src/features/post/PostsList.jsx
+++ client/src/features/post/PostsList.jsx
@@ -1,9 +1,29 @@
+import {useEffect, useState} from "react"
 
-function PostsList(props){
+function PostsList() {
+  const [postsList] = useState([
+    {
+      id: 1,
+      title: "title",
+      body: "test test test test"
+    }
+  ])
+
+  useEffect(() => {
+    // Fetch posts from API and update state
+    alert("112")
+  }, []);
+
   return (
     <>
       <h1>Posts List</h1>
       <p>List of posts will be displayed here.</p>
+      { postsList.map((post) => {
+        <div key={post.id} className={"post-item inline-flex"}>
+          <span> {`${post.title} body: ${post.body}`}</span>
+        </div>
+      })
+      }
     </>
   )
 }
