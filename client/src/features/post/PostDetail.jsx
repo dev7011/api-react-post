0a36bbb344ec96ccbe387403d67d8e32c751e5f3
ntle <tnle2017@gmail.com>
2026-08-10 16:08:07 +0200

    refactor: rename loadAllPosts function to loadPosts in PostsList component

--- client/src/features/post/PostDetail.jsx
+++ client/src/features/post/PostDetail.jsx
@@ -1,7 +1,31 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {fetchPost} from "../../services/postService.js";
+
 function PostDetail() {
+  const {id}= useParams()
+
+  const [post, setPost] = useState(null)
+
+  useEffect(() => {
+    ( async function eventShow(){
+      try {
+        const post = await fetchPost(id)
+        setPost(post)
+
+      } catch (error) {
+        console.error("Error fetching post:", error)
+      }
+    })()
+  }, [])
+
+
   return (
-    <>
+    post && <>
       <h1>Posts Detail</h1>
+      <h2>{`${post.id}. ${post.title}`}</h2>
+      <p>Body-text</p>
+      <p>{post.body}</p>
     </>
   )
 }
